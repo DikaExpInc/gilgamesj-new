8fa1af74e9ac2c9afce005d0f0d24343c5840e2e
DikaExpInc <dika@expinc.io>
2023-09-06 16:20:55 +0700

    fix all

--- app/lib/app/modules/pre_game_descibel_game/views/pre_game_descibel_game_view.dart
+++ app/lib/app/modules/pre_game_descibel_game/views/pre_game_descibel_game_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 
 import 'package:get/get.dart';
-import 'package:liquid_progress_indicator_v2/liquid_progress_indicator.dart';
 
 import '../controllers/pre_game_descibel_game_controller.dart';
 
